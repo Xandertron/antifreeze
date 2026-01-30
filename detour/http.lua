56eed9845b925b52a8ed9f5e3b1bfbb3763a7cb0
Xandertron <66480163+Xandertron@users.noreply.github.com>
2026-01-29 23:24:40 -0600

    Remove deprecated code

--- detour/http.lua
+++ detour/http.lua
@@ -3,9 +3,6 @@ local HWL = lje.require("service/http_whitelist.lua")
 local origHttp = HTTP
 
 local function httpHk(params)
-	lje.hooks.disable()
-	lje.env.disable_metatables()
-
 	local url = rawget(params, "url") or ""
 
 	if type(url) ~= "string" then
@@ -16,13 +13,8 @@ local function httpHk(params)
 
 	if not HWL.isWhitelisted(url) then
 		lje.con_printf("[AF] [HTTP] Blocked HTTP request to URL: $red{%s}", url)
-		lje.env.enable_metatables()
-		lje.hooks.enable()
 		return true -- make them think it was queued
 	end
-
-	lje.env.enable_metatables()
-	lje.hooks.enable()
 
 	return origHttp(params)
 end
@@ -31,18 +23,13 @@ rawset(_G, "HTTP", lje.detour(origHttp, httpHk))
 
 local origPanelOpenURL = FindMetaTable("Panel").OpenURL
 local function panelOpenUrlHk(self, url)
-	lje.hooks.disable()
-	lje.env.disable_metatables()
 	lje.con_printf("[AF] [Panel:OpenURL] Attempt to open URL: $yellow{%s}", url)
 	if HWL.isWhitelisted(url) then
 		lje.con_printf("[AF] [Panel:OpenURL] Allowing URL: $yellow{%s}", url)
-		lje.env.enable_metatables()
-		lje.hooks.enable()
 		return origPanelOpenURL(self, url)
 	else
 		lje.con_printf("[AF] [Panel:OpenURL] Blocking URL: $red{%s}", url)
-		lje.env.enable_metatables()
-		lje.hooks.enable()
+
 		return
 	end
 end
@@ -51,18 +38,14 @@ FindMetaTable("Panel").OpenURL = lje.detour(origPanelOpenURL, panelOpenUrlHk)
 
 local origGuiOpenURL = gui.OpenURL
 local function guiOpenUrlHk(url)
-	lje.hooks.disable()
-	lje.env.disable_metatables()
 	lje.con_printf("[AF] [gui.OpenURL] Attempt to open URL: $yellow{%s}", url)
 	if HWL.isWhitelisted(url) then
 		lje.con_printf("[AF] [gui.OpenURL] Allowing URL: $yellow{%s}", url)
-		lje.env.enable_metatables()
-		lje.hooks.enable()
+
 		return origGuiOpenURL(url)
 	else
 		lje.con_printf("[AF] [gui.OpenURL] Blocking URL: $red{%s}", url)
-		lje.env.enable_metatables()
-		lje.hooks.enable()
+
 		return
 	end
 end
